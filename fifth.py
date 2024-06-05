2f23f81545d5a811ca8514989fd5b53813554034
Rtksharma <105188933+Rtksharma@users.noreply.github.com>
2024-06-05 11:19:03 +0530

    Create fifth.py

---
+++ fifth.py
@@ -0,0 +1,7 @@
+def merge_the_tools(string, k):
+    for i in range(0, len(string), k):
+        substr = []  
+        for j in range(i, i+k):
+            if string[j] not in substr:
+                substr.append(string[j])
+        print(''.join(substr))
